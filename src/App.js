e5576cf9800b9cb95a6a10d79f1d344ca758d2dc
yrcmpbll <yuri.campbell@gmail.com>
2022-08-28 09:33:55 +0000

    start commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 function App() {
   return (
     <div className="App">
-      
+      <header>
+        <h1>
+          Seeker
+        </h1>
+        <form className="search-box">
+          <input type="search" placeholder="What do you search?" />
+        </form>
+        <p>Search hits: 0</p>
+        <div className="results">
+          <div className="result">
+            <h3>Title goes here</h3>
+            <p>Lorem ipsum sit and so on.</p>
+            <a href="#">Read</a>
+          </div>
+        </div>
+      </header>
     </div>
   );
 }
